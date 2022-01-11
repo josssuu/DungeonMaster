65fd99763a6f34698a91670fd88f84dc226c36f1
poolaandreas <poolaandreas@gmail.com>
2022-01-11 15:35:41 +0200

    JumpHalfway i#

--- Assets/Scripts/Game.cs
+++ Assets/Scripts/Game.cs
@@ -8,15 +8,18 @@ public class Game : NetworkBehaviour
 
     public GameObject TreePrefab;
     public GameObject BlockPrefab;
-    public GameObject Upgrade;
+    public GameObject SpeedUpgrade;
+    public GameObject JumpUpgrade;
     public float TreeDistance = 1f;
     public float BlockDistance = 1f;
     public float UpgradeDistance = 1f;
+    public float UpgradeDistance2 = 2f;
 
     public float Speed = 1f;
     private List<GameObject> _trees;
     private List<GameObject> _blocks;
     private List<GameObject> _upgrades;
+    private List<GameObject> _upgradesJump;
     private bool _stop;
 
     void Start()
@@ -26,6 +29,7 @@ public class Game : NetworkBehaviour
         _trees = new List<GameObject>();
         _blocks = new List<GameObject>();
         _upgrades = new List<GameObject>();
+        _upgradesJump = new List<GameObject>();
         for (int i = 0; i < 16; i++)
         {
             GameObject tree = Instantiate(TreePrefab);
@@ -42,11 +46,19 @@ public class Game : NetworkBehaviour
 
             if (i == 4 | i == 14)
             {
-                GameObject upgrade = Instantiate(Upgrade);
+                
+                GameObject upgrade = Instantiate(SpeedUpgrade);
                 //upgrade.GetComponent<NetworkObject>().Spawn();
 
                 _blocks.Add(upgrade);
                 upgrade.transform.position = new Vector3(UpgradeDistance * i * 3f, Random.Range(-4f, -1.2f), 0);
+            }
+            if(i==6|| i == 16)
+            {
+                GameObject jump = Instantiate(JumpUpgrade);
+                _blocks.Add(jump);
+                jump.transform.position = new Vector3(UpgradeDistance2 * i * 3f, Random.Range(-4f, -1.2f), 0);
+
             }
         }
     }
@@ -57,6 +69,7 @@ public class Game : NetworkBehaviour
             Speed += Time.deltaTime * 0.2f;
             BlockDistance = Random.Range(5f, 15f);
             UpgradeDistance = Random.Range(5f, 15f);
+            UpgradeDistance2 = Random.Range(4f, 16f);
 
             foreach (GameObject tree in _trees)
             {
@@ -90,6 +103,14 @@ public class Game : NetworkBehaviour
                     upgrade.transform.position += new Vector3(UpgradeDistance * _upgrades.Count, 0f, 0f);
                 }
             }
+            foreach (GameObject jump in _upgradesJump)
+            {
+                jump.transform.position -= new Vector3(Time.deltaTime * Speed, 1f, 1f);
+                if (jump.transform.position.x < -UpgradeDistance2 * (_upgradesJump.Count / 2.5f))
+                {
+                    jump.transform.position += new Vector3(UpgradeDistance2 * _upgradesJump.Count, 1f, 1f);
+                }
+            }
         }
     }
 
@@ -109,6 +130,11 @@ public class Game : NetworkBehaviour
         }
 
         foreach (GameObject upgrade in _upgrades)
+        {
+            //upgrade.GetComponent<NetworkObject>().Despawn(true);
+            Destroy(upgrade);
+        }
+        foreach (GameObject upgrade in _upgradesJump)
         {
             //upgrade.GetComponent<NetworkObject>().Despawn(true);
             Destroy(upgrade);
